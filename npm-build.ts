7ffcbfc0f7cb68cd210ec8352d6a659f8952bb86
vuongtm181293 <vuongtm181293@gmail.com>
2019-11-19 09:59:00 +0700

    no message

---
+++ npm-build.ts
@@ -0,0 +1,20 @@
+//Icon
+https://www.npmjs.com/package/@coreui/icons
+
+npm i @coreui/icons
+
+//simple-line-icons
+https://www.npmjs.com/package/simple-line-icons
+
+npm i simple-line-icons
+
+
+//ngx-toastr
+https://www.npmjs.com/package/ngx-toastr
+
+npm i ngx-toastr
+
+//font-awesom
+https://www.npmjs.com/package/font-awesome
+
+npm i font-awesome
